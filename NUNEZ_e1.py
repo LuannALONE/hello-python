de3e2cdf2e7a90ad43dc1330802629da90aa0741
LuannALONE <44455720+LuannALONE@users.noreply.github.com>
2018-10-25 16:46:10 +0800

    Create NUNEZ_e1.py

---
+++ NUNEZ_e1.py
@@ -0,0 +1,5 @@
+I am Luisa-Ann Nunez
+My Spirit Animal is Tiger
+because because like a tiger , for me ,i can say that i am brave to face those who does hurt not only to me ,to my love ones rather
+When not in school,I love to to do jogging arround @ 4-5:00 in the morning
+I dream of being a/an Police or Philippine Army
